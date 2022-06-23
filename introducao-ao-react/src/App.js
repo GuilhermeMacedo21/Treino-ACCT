de70875c3a43a8ad8f607a4581e657a97c089459
Guilherme Macedo <guilherme.macedo@acct.global>
2022-06-23 11:21:02 -0300

    5 Hello World - Função de alterar entre maisculo e minusculo

--- introducao-ao-react/src/App.js
+++ introducao-ao-react/src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 
 const helloWorld = "Hello World!"
 const data = new Date().toDateString()
+function helloWorldEmbaralhado(string){
+    let embaralhado = string.split('');
+    let cont = 0;
+    while(cont<12){
+        if(cont%2 == 1){
+          embaralhado[cont] = embaralhado[cont].toUpperCase();
+        }else{
+          embaralhado[cont] = embaralhado[cont].toLowerCase();
+        }
+        cont++;
+    }
+    return embaralhado;
+}
 function App() {
   return (
     <>
-    <h2>{helloWorld}</h2><p> Data: {data}</p>
-    <h2>{helloWorld.toUpperCase()}</h2><p> Data: {data}</p>
-    <h2>{helloWorld.toLowerCase()}</h2><p> Data: {data}</p>
-    <h2>{helloWorld.split('').reverse().join('')}</h2><p> Data: {data}</p>
+    <div className="format"><h2>{helloWorld}</h2><p> Data: {data}</p></div>
+    <div className="format"><h2>{helloWorld.toUpperCase()}</h2><p> Data: {data}</p></div>
+    <div className="format"><h2>{helloWorld.toLowerCase()}</h2><p> Data: {data}</p></div>
+    <div className="format"><h2>{helloWorld.split('').reverse().join('')}</h2><p> Data: {data}</p></div>
+    <div className="format"><h2>{helloWorldEmbaralhado("Hello World!")}</h2><p> Data: {data}</p></div>
     </>
   );
 }

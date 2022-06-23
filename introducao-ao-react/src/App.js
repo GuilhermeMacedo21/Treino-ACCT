1308539308856ea1a5655a1790196002db75151b
Guilherme Macedo <guilherme.macedo@acct.global>
2022-06-23 11:48:31 -0300

    7 Hello World - Alterando fundo com CSS

--- introducao-ao-react/src/App.js
+++ introducao-ao-react/src/App.js
@@ -23,10 +23,16 @@ function App() {
     <div className="format"><h2>{helloWorld.toUpperCase()}</h2><p> Data: {data}</p></div>
     <div className="format"><h2>{helloWorld.toLowerCase()}</h2><p> Data: {data}</p></div>
     <div className="format"><h2>{helloWorld.split('').reverse().join('')}</h2><p> Data: {data}</p></div>
-    <div className="format"><h2>{helloWorldEmbaralhado("Hello World!")}</h2><p> Data: {data}</p></div>
+    <div className="format"><h2>{helloWorldEmbaralhado(helloWorld)}</h2><p> Data: {data}</p></div>
     <div className="format"><h2>{helloWorldEmbaralhado(helloWorld.split('').reverse().join(''))}</h2><p> Data: {data}</p></div>
+    <div className="format" id="world7"><h2>Hello World!</h2><button onClick={alteraFundo}>Alterar</button><p> Data: {data}</p></div>
     </>
   );
 }
-
+function alteraFundo(){
+  let elem = document.getElementById("world7")
+  if(elem){
+    elem.classList.toggle("fundo");
+  }  
+}
 export default App;

b8839fc88a1ffdff29cd024d897f785ef657d1e3
Guilherme Macedo <guilherme.macedo@acct.global>
2022-06-23 10:42:34 -0300

    2 Hello World - UpperCase

--- introducao-ao-react/src/App.js
+++ introducao-ao-react/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const helloWorld = "Hello World!"
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <>
+    <h2>{helloWorld}</h2>
+    <h2>{helloWorld.toUpperCase()}</h2>
+    </>
   );
 }
 
